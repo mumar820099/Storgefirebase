b5e58c1293093114caa7a300310df4cb99cefb74
Muhammad Umar <mumar820099@gmai.com>
2020-03-30 21:53:25 +0500

    Initial commit

---
+++ app/src/main/java/com/example/storgefirebase/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.storgefirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+
+            db = FirebaseFirestore.getInstance();
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Error Initializing "+e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void toupload(View v)
+    {
+        Intent intent  = new Intent(getBaseContext(),UploadImage.class);
+        startActivity(intent);
+    }
+    public void touploadVideo(View v)
+    {
+        Intent intent  = new Intent(getBaseContext(),VideoUpload.class);
+        startActivity(intent);
+    }
+    public void todownloadVideo(View v)
+    {
+        Intent intent  = new Intent(getBaseContext(),VideoDownlaod.class);
+        startActivity(intent);
+    }
+    public void todownload(View v)
+    {
+        Intent intent  = new Intent(getBaseContext(),DownloadImage.class);
+        startActivity(intent);
+    }
+    public void adduser(View v) {
+        try {
+
+            Map<String, Object> datamap = new HashMap<>();
+            datamap.put("address", "Lahore");
+            db.collection("Bookings").document("Booking1").set(datamap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(MainActivity.this, "Data Added Successfully", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Data Not Added Successfully" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(this, "Error in adding user to firebase " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
